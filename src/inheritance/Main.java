6ae7480716cee8d667928085088a3b8220ae6758
CoochiecooBerrauslu <113683902+CoochiecooBerrauslu@users.noreply.github.com>
2022-09-28 16:19:15 +0300

    Update Main.java

--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -2,7 +2,7 @@ package inheritance;
 
 public class Main {
     public static void main(String[] args){
-        //iki nesne arasında miras durumu bir nesne diğerine miğras bırakır.
+        //iki nesne arasında miras durumu bir nesne diğerine miras bırakır.
    Customer customer = new Customer();
    Employee employee = new Employee();
 
